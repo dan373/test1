075cec4c72a9680b4067993461d39ec5a991cbba
dan373 <54936191+dan373@users.noreply.github.com>
2019-09-05 12:49:38 +0200

    Add files via upload

---
+++ status.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import os
+import commands
+import sys
+
+service_name = "httpd"
+def check_service_status(service_name):
+
+        status = os.system('service '+service_name+ ' status > /dev/null')
+        return status
+file = open('/home/centos/stav.txt','a')
+
+
+def main():
+
+        if (check_service_status(service_name) == 0):
+                st = "HTTPD UP!! "
+                file.write(st + '\n')
+                file.close()
+        else:
+                st = "HTTPD DOWN!! "
+                file.write(st + '\n')
+                file.close()
+main()
+
+
+
+service_name1 = "mysqld"
+def check_service_status(service_name1):
+
+        status = os.system('service '+service_name1+ ' status > /dev/null')
+        return status
+
+file = open('/home/centos/stav.txt','a')
+
+def main():
+
+         if (check_service_status(service_name1) == 0):
+                st = "MYSQL UP!!"
+                file.write(st + '\n')
+                file.close()
+         else:
+                st = "MYSQL DOWN!!"
+                file.write(st + '\n')
+                file.close()
+main()
